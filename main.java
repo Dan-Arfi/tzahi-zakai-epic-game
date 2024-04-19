219569e7fbaa7d852ba62e7fbc1b86e718c7d232
Dan-Arfi <danarfi07@gmail.com>
2024-04-19 12:15:05 +0300

    anongus

--- main.java
+++ main.java
@@ -15,9 +15,22 @@ public class main {
     }
 
     public static void playGame() {
+        char currentPlayer = 'x';
+        boolean end = false;
         char[][] board = new char[3][3];
         initBoard(board);
-        drawBoard(board);
+
+        while (!end) {
+            drawBoard(board);
+
+            makeMove(currentPlayer, board);
+            // checkForWinner(0, board);
+            if (currentPlayer == 'x') {
+                currentPlayer = 'o';
+            } else {
+                currentPlayer = 'x';
+            }
+        }
 
     }
 
@@ -34,6 +47,9 @@ public class main {
     }
 
     public static void drawBoard(char[][] board) {
+        // clear console
+        System.out.print("\033[H\033[2J");
+
         char[] letters = { 'a', 'b', 'c' };
         for (int y = 0; y <= board[0].length; y++) {
             for (int x = 0; x <= board[0].length; x++) {
@@ -43,16 +59,79 @@ public class main {
 
                     System.out.print("| " + letters[y - 1] + " ");
 
+                } else if (y == 0 && x == 0) {
+                    System.out.print("|  ");
                 } else {
-                    System.out.print("|   ");
+
+                    System.out.print("| " + board[x - 1][y - 1]);
+
                 }
 
             }
 
             System.out.println("");
             System.out.println("----------------");
-            
+
         }
+
+    }
+
+    public static void makeMove(char player, char[][] board) {
+        System.out.println("where do you want to place " + player + " on the x axis? (1 - 3)");
+        int xPos = reader.nextInt();
+        System.out.println("where do you want to place " + player + " on the y axis? (a - c)");
+        char yPos = reader.next().charAt(0);
+        int y = 0;
+        xPos -= 1;
+        if (yPos == 'a') {
+            y = 0;
+        } else if (yPos == 'b') {
+            y = 1;
+        } else if (yPos == 'c') {
+            y = 2;
+        }
+        if (checkIfValid(yPos, xPos, board)) {
+            board[xPos][y] = player;
+        } else {
+            System.out.println("no sir");
+        }
+
+    }
+
+    public static boolean checkIfValid(char yPos, int xPos, char[][] board) {
+        int y = 0;
+        if (yPos == 'a') {
+            y = 0;
+        } else if (yPos == 'b') {
+            y = 1;
+        } else if (yPos == 'c') {
+            y = 2;
+        }
+        if (board[xPos][y] == ' ') {
+            return true;
+        } else {
+            System.out.println("sorry! place " + xPos + "" + yPos + " is not available :(");
+            return false;
+        }
+    }
+
+    public static void checkForWinner(char player, char[][] board) {
+        // check for the wiiner
+        /*
+         * 
+         * פעולה 7 - פעולת checkForWinner :
+         * • הפעולה מקבלת כפרמטרים:
+         * .1 את לוח המשחק
+         * .2 את סימן השחקן הנוכחי )X או O)
+         * 
+         * • תפקיד הפעולה הוא לבדוק האם השחקן ניצח.
+         * • קריטריונים לניצחון )מספיק אחד מהשלושה(:
+         * .1 לשחקן יש שלושה סימונים שלו באותה השורה
+         * .2 לשחקן יש שלושה סימונים שלו באותה העמודה
+         * .3 לשחקן יש שלושה סימונים שלו באחד האלכסונים
+         * 
+         * • הפעולה תחזיר true במידה והשחקן ניצח, אחרת תחזיר false
+         */
     }
 
 }
